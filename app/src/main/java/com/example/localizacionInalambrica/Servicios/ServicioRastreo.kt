47663cc2234f43b67c47d30bc9b88fb7f241a516
DarkWayC0de <darkwaycodeohk@gmail.com>
2021-08-26 21:47:50 +0100

    Servicio Actualizacion notifiaciones

--- app/src/main/java/com/example/localizacionInalambrica/Servicios/ServicioRastreo.kt
+++ app/src/main/java/com/example/localizacionInalambrica/Servicios/ServicioRastreo.kt
@@ -1,6 +1,5 @@
 package com.example.localizacionInalambrica.Servicios
 
-import android.Manifest
 import android.annotation.SuppressLint
 import android.app.NotificationChannel
 import android.app.NotificationManager
@@ -10,54 +9,63 @@ import android.app.PendingIntent.FLAG_UPDATE_CURRENT
 import android.content.Context
 import android.content.Intent
 import android.location.Location
-import android.os.Build
 import android.os.Looper
 import android.util.Log
-import androidx.annotation.RequiresApi
 import androidx.core.app.NotificationCompat
 import androidx.lifecycle.LifecycleService
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
-import com.example.localizacionInalambrica.*
 import com.example.localizacionInalambrica.R
 import com.example.localizacionInalambrica.other.Constants.ACTION_PAUSE_SERVICE
 import com.example.localizacionInalambrica.other.Constants.ACTION_START_OR_RESUME_SERVICE
 import com.example.localizacionInalambrica.other.Constants.ACTION_STOP_SERVICE
-import com.example.localizacionInalambrica.other.Constants.NOTIFICATION_CHANEL_ID
-import com.example.localizacionInalambrica.other.Constants.NOTIFICATION_CHANEL_NAME
-import com.example.localizacionInalambrica.other.Constants.ACTION_SHOW_HOME_FRAGMEWNT
 import com.example.localizacionInalambrica.other.Constants.FASTEST_LOCATION_INTERVAL
 import com.example.localizacionInalambrica.other.Constants.LOCATION_UPDATE_INTERVAL
+import com.example.localizacionInalambrica.other.Constants.NOTIFICATION_CHANEL_ID
+import com.example.localizacionInalambrica.other.Constants.NOTIFICATION_CHANEL_NAME
 import com.example.localizacionInalambrica.other.Constants.NOTIFICATION_ID
 import com.example.localizacionInalambrica.permisos.permissions
-import com.google.android.gms.location.*
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
 import com.google.android.gms.location.LocationRequest.PRIORITY_HIGH_ACCURACY
+import com.google.android.gms.location.LocationResult
+import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
-
+@AndroidEntryPoint
 class ServicioRastreo : LifecycleService() {
     private val TAG = "ServicioRastreo"
     private var isFirstRun = true
+
+    @Inject
     lateinit var fusedLocationProviderClient: FusedLocationProviderClient
 
+    @Inject
+    lateinit var baseNotificationBuilder: NotificationCompat.Builder
+
+    lateinit var actualNotificationBuilder: NotificationCompat.Builder
 
 
-    companion object{
-       val  isTracking = MutableLiveData<Boolean>()
-       val actualPosition = MutableLiveData<Location?>()
+    companion object {
+        val isTracking = MutableLiveData<Boolean>()
+        val actualPosition = MutableLiveData<Location?>()
 
     }
-    private fun postInitalValues(){
+
+    private fun postInitalValues() {
         isTracking.postValue(false)
         actualPosition.postValue(null)
     }
 
     override fun onCreate() {
         super.onCreate()
+        actualNotificationBuilder = baseNotificationBuilder
         postInitalValues()
         fusedLocationProviderClient = FusedLocationProviderClient(this)
         isTracking.observe(this, Observer {
             updateLocationTracking(it)
+            actualizarNotificacionUbicacion(it)
         })
 
     }
@@ -92,18 +100,25 @@ class ServicioRastreo : LifecycleService() {
         isTracking.postValue(true)
 
         val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE)
-            as NotificationManager
+                as NotificationManager
 
         crearNotificationChannel(notificationManager)
-        val notificationBuilder = NotificationCompat.Builder(this, NOTIFICATION_CHANEL_ID)
-            .setAutoCancel(false)
-            .setOngoing(true)
-            .setSmallIcon(R.drawable.ic_my_location_24)
-            .setContentTitle(getString(R.string.tituloServicioRastreoNotificacion))
-            .setContentText("0,0")
-            .setContentIntent(getMainActivityPendingIntent())
 
-        startForeground(NOTIFICATION_ID,notificationBuilder.build())
+        startForeground(NOTIFICATION_ID, baseNotificationBuilder.build())
+
+        actualPosition.observe(
+            this, Observer {
+                if (it != null) {
+                    val notification = actualNotificationBuilder
+                        .setContentText(
+                            "Log: " + it!!.longitude.toString() + ", Lat: " + it.latitude.toString() + "\n"
+                                    + "Alt: " + it.altitude.toString()
+                        )
+                    notificationManager.notify(NOTIFICATION_ID, notification.build())
+                }
+            }
+
+        )
 
     }
     private fun pauseService() {
@@ -151,22 +166,46 @@ class ServicioRastreo : LifecycleService() {
 
     }
 
-    private fun getMainActivityPendingIntent()  = PendingIntent.getActivity(
-        this,
-        0,
-        Intent(this, MainActivity::class.java).also{
-            it.action = ACTION_SHOW_HOME_FRAGMEWNT
-        },
-        FLAG_UPDATE_CURRENT
-    )
 
-    private fun crearNotificationChannel(notificationmanager :NotificationManager){
+    private fun crearNotificationChannel(notificationmanager: NotificationManager) {
         val channel = NotificationChannel(
             NOTIFICATION_CHANEL_ID,
             NOTIFICATION_CHANEL_NAME,
             IMPORTANCE_LOW
         )
         notificationmanager.createNotificationChannel(channel)
+    }
+
+    private fun actualizarNotificacionUbicacion(isTracking: Boolean) {
+        val notificationActionText =
+            if (isTracking) {
+                getString(R.string.botonCerrarNotificacion)
+            } else {
+                getString(R.string.botonContinuarNotificacion)
+            }
+
+        val pendingIntent =
+            if (isTracking) {
+                val cerrarIntent = Intent(this, ServicioRastreo::class.java).apply {
+                    action = ACTION_STOP_SERVICE
+                }
+                PendingIntent.getService(this, 1, cerrarIntent, FLAG_UPDATE_CURRENT)
+            } else {
+                val comenzarIntent = Intent(this, ServicioRastreo::class.java).apply {
+                    action = ACTION_START_OR_RESUME_SERVICE
+                }
+                PendingIntent.getService(this, 1, comenzarIntent, FLAG_UPDATE_CURRENT)
+            }
+        val notificationManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        actualNotificationBuilder.javaClass.getDeclaredField("mActions").apply {
+            isAccessible = true
+            set(actualNotificationBuilder, ArrayList<NotificationCompat.Action>())
+        }
+        actualNotificationBuilder = baseNotificationBuilder
+            .addAction(R.drawable.ic_pause_black_24dp, notificationActionText, pendingIntent)
+        notificationManager.notify(NOTIFICATION_ID, actualNotificationBuilder.build())
     }
 
 }
